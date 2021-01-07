754c08bab415abd114c21ab3a04a67f5c4289ad3
plun1331 <plun1331@gmail.com>
2021-01-07 15:57:22 -0800

    new command manager

--- CommandsRewrite/__init__.py
+++ CommandsRewrite/__init__.py
@@ -4,23 +4,177 @@ from podrum.command.CommandManager import CommandManager
 from podrum.utils.Utils import Utils
 from podrum.plugin.Plugin import Plugin
 from podrum.command.CommandManager import CommandManager
-# Default Commands
-from podrum.command.vanilla.DifficultyCommand import DifficultyCommand
-from podrum.command.vanilla.HelpCommand import HelpCommand
-from podrum.command.vanilla.PluginsCommand import PluginsCommand
-from podrum.command.vanilla.ReloadCommand import ReloadCommand
-from podrum.command.vanilla.SayCommand import SayCommand
-from podrum.command.vanilla.StopCommand import StopCommand
-# Rewritten Commands
-from .commands import StopCommandR
-from .commands import SayCommandR
-from .commands import ReloadCommandR
-from .commands import PluginsCommandR
-from .commands import HelpCommandR
-from .commands import DifficultyCommandR
-from .commands import LoadPlugin
-from .commands import UnloadPlugin
-from .commands import DebugCommand
+from podrum.lang.Base import Base
+from .CommandManager import CommandManager2
+# External Modules
+from zipfile import ZipFile
+import json
+import traceback
+import time
+import PluginAPI
+import sys
+
+CommandManager2 = CommandManager2()
+plugin = PluginAPI.Plugins()
+
+@plugin.command(
+    description = "Stops the server."
+)
+def stop(sender):
+    sender.sendMessage("Stopping server...")
+    Plugin.unloadAll()
+    sender.sendMessage("Server stopped.")
+    Utils.killServer()
+
+@plugin.command(
+    description = "Says something in chat."
+)
+def say(sender, *, message):
+    sender.sendMessage(message)
+
+@plugin.command(
+    description = "Reloads all plugins."
+)
+def reload(sender):
+    sender.sendMessage("Reloading...")
+    try:
+        Plugin.unloadAll()
+        Plugin.loadAll()
+    except Exception as e:
+        sender.sendMessage(f"Plugins could not be reloaded: {e}")
+    sender.sendMessage("Reload successful!")
+
+@plugin.command(
+    description = 'Shows information on a plugin.'
+)
+def plugins(sender, *, plugin = None):
+    if plugin is not None:
+        if plugin in Plugin.plugins:
+            _plugin = Plugin.plugins[plugin]
+            sender.sendMessage(f"--- Showing info on plugin {plugin} ---")
+            sender.sendMessage(_plugin["description"])
+            sender.sendMessage(f"Author: {_plugin['author']}")
+            sender.sendMessage(f"Version {_plugin['version']}")
+            return
+        else:
+            sender.sendMessage("Invalid plugin.")
+            return
+    pluginsString = ""
+    for count, pluginName in enumerate(Plugin.plugins):
+        pluginsString += pluginName
+        if count >= Plugin.pluginsCount:
+            pluginsString += ", "
+    sender.sendMessage(f"Plugins({Plugin.pluginsCount}): {pluginsString}")
+
+@plugin.command(
+    description = 'Shows help for a command.'
+)
+def help(sender, *, command=None):
+    if command is None:
+        sender.sendMessage("--- Showing help ---")
+        for k, v in CommandManager.commands.items():
+            sender.sendMessage("/" + str(k) + ": " + str(v.description))
+        return
+    command = CommandManager2.getCommand(command)
+    if command is not None:
+        sender.sendMessage(f"--- Showing help for {command.name} ---")
+        sender.sendMessage(f"{command.description}")
+        sender.sendMessage(f"Usage: /{command.name} {command.usage}")
+    else:
+        sender.sendMessage("Invalid command.")
+
+
+@plugin.command(
+    description = 'Changes the game difficulty.'
+)
+def difficulty(sender, *, difficulty):
+    if difficulty == "0" or difficulty.lower() == "peaceful":
+        sender.sendMessage(f"This command does nothing right now lol")
+    elif difficulty == "1" or difficulty.lower() == "easy":
+        sender.sendMessage(f"This command does nothing right now lol")
+    elif difficulty == "2" or difficulty.lower() == "normal":
+        sender.sendMessage(f"This command does nothing right now lol")
+    elif difficulty == "3" or difficulty.lower() == "hard":
+        sender.sendMessage(f"This command does nothing right now lol")
+    else:
+        sender.sendMessage(f"{difficulty} is not a valid parameter!")
+
+@plugin.command(
+    description = 'Loads a plugin. The plugin argument is the path to the plugin (e.g. plugins/plugin.pyz)'
+)
+def load(sender, *, plugin):
+    try:
+        plugin = ZipFile(plugin, "r")
+        pluginInfo = json.loads(plugin.read("plugin.json"))
+        if pluginInfo["name"] in Plugin.plugins:
+            sender.sendMessage("Cannot load duplicate plugin " + pluginInfo["name"])
+            return
+    except:
+        sender.sendMessage("Invalid plugin.")
+        return
+    try:
+        Plugin.load(plugin)
+    except:
+        tb = traceback.format_exc()
+        sender.sendMessage.error(f"Error loading plugin {plugin}")
+        for line in tb.split('\n'):
+            sender.sendMessage(line)
+        return
+    sender.sendMessage(pluginInfo["name"] + " loaded.")
+
+@plugin.command(
+    description = 'Unloads a plugin. The plugin argument is the name of the plugin.'
+)
+def unload(sender, *, plugin):
+    if plugin in Plugin.plugins:
+        try:
+            Plugin.unload(plugin)
+        except:
+            tb = traceback.format_exc()
+            sender.sendMessage(f"Error unloading plugin {plugin}")
+            for line in tb.split('\n'):
+                sender.sendMessage(line)
+            return
+    else:
+        sender.sendMessage(plugin + " is not loaded.")
+        return
+    sender.sendMessage(plugin + " unloaded.")
+
+@plugin.command(
+    description = 'Debugs a command.'
+)
+def debug(sender, command, *args):
+    arg = [command]
+    arg.extend(args)
+    if command.lower() == 'debug':
+        sender.sendMessage("Cannot debug the debug command.")
+        return
+    sender.sendMessage(f"Debugging command '{command}':")
+    onExcecute = time.time()
+    sender.sendMessage("")
+    try:
+        if CommandManager.isCommand(command):
+            CommandManager.commands[command].execute(sender, arg)
+        else:
+            sender.sendMessage(Base.getTranslation("invalidCommand"))
+    except:
+        tb = traceback.format_exc()
+        for line in tb.split('\n'):
+            sender.sendMessage(line)
+        return
+    end = time.time()
+    sender.sendMessage("")
+    sender.sendMessage(f"Execution completed in {end-onExcecute} seconds.")
+
+@plugin.event()
+def on_command_error(command, sender, args, error):
+    if isinstance(error, PluginAPI.MissingRequiredArgument) or isinstance(error, PluginAPI.TooManyArguments):
+        return sender.sendMessage(f"Usage: /{command.name} {command.usage}")
+    if isinstance(error, TypeError):
+        return sender.sendMessage(f"Something went wrong (you probably provided too many/too little arguments)")
+    print(f"Ignoring exception in {command.name}:")
+    traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+
 
 #core class
 class CommandsRewrite:
@@ -31,53 +185,12 @@ class CommandsRewrite:
     version = "v1.2"
     server = None
 
-    def __init__(self):
-        self.commands = [StopCommandR(),
-                         SayCommandR(),
-                         HelpCommandR(),
-                         ReloadCommandR(),
-                         PluginsCommandR(),
-                         DifficultyCommandR(),
-                         LoadPlugin(),
-                         UnloadPlugin(),
-                         DebugCommand()]
-        self.default_commands_str = ["stop",
-                                "say",
-                                "reload",
-                                "plugins",
-                                "help",
-                                "difficulty"]
-        self.commands_str = ["stop",
-                        "say",
-                        "reload",
-                        "plugins",
-                        "help",
-                        "difficulty",
-                        "load",
-                        "unload",
-                        "debug"]
-        self.default_commands = [DifficultyCommand(),
-                            HelpCommand(),
-                            PluginsCommand(),
-                            ReloadCommand(),
-                            SayCommand(),
-                            StopCommand()]
-
     
     def onLoad(self):
-        """ Called when the plugin is loaded. """
         pass
 
     def onEnable(self):
-        """ Removes the default commands and replaces them with better commands, as well as adds some new commands. """
-        for command in self.default_commands_str:
-            CommandManager.deleteCommand(command)
-        for command in self.commands:
-            CommandManager.registerCommand(command)
+        plugin.startup()
 
     def onDisable(self):
-        """ Removes the commands in the plugin, and reinstates the default commands. """
-        for command in self.commands_str:
-            CommandManager.deleteCommand(command)
-        for command in self.default_commands:
-            CommandManager.registerCommand(command)
+        plugin.cleanup()
--- README.md
+++ README.md
@@ -3,12 +3,12 @@ A Podrum plugin that replaces the default commands, as well as adds some new one
 
 The [Podrum software](https://github.com/Podrum/Podrum) is currently a work in progress, so currently this is useless.
 
+This plugin requires [PodrumPluginAPI](https://github.com/plun1331/PodrumPluginAPI). You can get
+
 # How to Install
 1. Go to the [releases section](https://github.com/plun1331/PodrumCommandRewrite/releases) and download the following files from the latest release:
   
   - `CommandsRewrite.pyz`
-  
-  - `CommandManager.py`
   
   ![image](https://user-images.githubusercontent.com/49261529/103855273-32426e00-5067-11eb-8d42-54b29dee27e6.png)
   
@@ -19,10 +19,5 @@ The [Podrum software](https://github.com/Podrum/Podrum) is currently a work in p
 
   ![image](https://user-images.githubusercontent.com/49261529/103855149-e55e9780-5066-11eb-8d5d-d1011eab92e6.png)
 
-
-3. Replace the file at `src\podrum\command\CommandManager.py` with your downloaded `CommandManager.py`.
-
-  ![image](https://user-images.githubusercontent.com/49261529/103855319-4d14e280-5067-11eb-8d2a-ff19e0acf5cd.png)
-
-4. Run the server.
+3. Run the server.
 
